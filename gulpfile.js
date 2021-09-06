2b367256847233d12ff70c07321727eafb6ad747
alucard <knyazev.activ@yandex.ru>
2021-09-07 00:46:06 +0300

    Завершает основную часть главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('css', function () {
 });
 
 gulp.task('scripts', function () {
-  return gulp.src('source/js/main.js')
+  return gulp.src('source/js/*.js')
       .pipe(gulp.dest('build/js'));
 });
 
@@ -51,7 +51,7 @@ gulp.task('server', function () {
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
   gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
-  gulp.watch('source/js/main.js', gulp.series('scripts', 'refresh'));
+  gulp.watch('source/js/*.js', gulp.series('scripts', 'refresh'));
 });
 
 gulp.task('refresh', function (done) {
--- source/js/main.js
+++ source/js/main.js
@@ -7,7 +7,74 @@ const pageHeaderLogo = document.querySelector('.page-header__logo');
 const pageHeaderCart = document.querySelector('.page-header__cart');
 const pageHeaderMenu = document.querySelector('.page-header__menu');
 const pageHeaderPromo = document.querySelector('.page-header__promo');
+const novelty = document.querySelector('.novelty');
+const noveltySlick = document.querySelector('.novelty__slick');
+const noveltyItems = document.querySelectorAll('.novelty__item');
+const noveltyArrows = document.querySelectorAll('.novelty__arrow');
+const noveltyCards = document.querySelector('.novelty__cards');
 const body = document.querySelector('.body');
+const questions = document.querySelector('.questions');
+const accordionTriggers = questions.querySelectorAll('button');
+
+document.addEventListener('DOMContentLoaded', function() {
+  $(".novelty__slick").slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    dots: true,
+    swipe: false,
+
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: true,
+          dots: true,
+          swipe: true,
+        }
+      }, {
+        breakpoint: 768,
+        settings: {
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          arrows: true,
+          dots: true,
+          swipe: true,
+        }
+      }
+    ]
+  });
+});
+
+if (novelty) {
+  novelty.classList.remove('novelty--nojs');
+}
+
+if (noveltySlick) {
+  noveltySlick.classList.remove('novelty__slick--nojs');
+}
+
+if (noveltyItems) {
+  noveltyItems.forEach(item => {
+    item.classList.remove('novelty__item--nojs');
+    item.classList.remove('novelty__item--tablet');
+    item.classList.remove('novelty__item--mobile')
+  });
+}
+
+if (noveltyArrows) {
+  noveltyArrows.forEach(arrow => {
+    arrow.classList.remove('novelty__arrow--nojs');
+  });
+}
+
+if (noveltyCards) {
+  noveltyCards.classList.remove('novelty__cards--nojs');
+}
 
 if (pageHeader) {
   pageHeader.classList.remove('page-header--nojs');
@@ -59,12 +126,27 @@ if (pageHeaderMenuButton) {
       pageHeaderMenu.classList.toggle('page-header__menu--opened');
     }
 
-    if(pageHeaderPromo) {
+    if (pageHeaderPromo) {
       pageHeaderPromo.classList.toggle('page-header__promo--opened');
     }
 
-    if(body) {
+    if (body) {
       body.classList.toggle('body--hidden');
     }
+  });
+}
+
+const onAccordionClick = (evt) => {
+  const id = evt.target.getAttribute('data-accordion');
+  const item = questions.querySelector('li[data-accordion="' + id + '"]');
+
+  if (item) {
+    item.classList.toggle('questions__item--show');
+  }
+};
+
+if (accordionTriggers) {
+  accordionTriggers.forEach(trigger => {
+    trigger.addEventListener('click', onAccordionClick);
   });
 }
